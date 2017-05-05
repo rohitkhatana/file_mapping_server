8d23c044b15b337eb770190a17cf41f8ce29a5c6
rohit khatana <rohitkhatana.khatana@gmail.com>
2017-05-05 16:16:02 +0530

    written an endpoint for mapping with mongo collection

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+import requests
+
+from config import Config
+from mongo import client
+
+
+config = Config().turtlemint
+db_obj = client(config['username'], config['password'], config['database'], host=config['host'])
+file_db = db_obj[config['database']]
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
-def hello():
-    return "Hello World!"
+
+def create_file_mapping(url, bucket_name):
+    req = requests.get(url)
+    content_type = req.headers['Content-Type']
+    return  {
+        'key': url.split('https://docs.turtlemint.com/')[1],
+        'bucketName': bucket_name,
+        'host': 'localhost',
+        'mimeType': content_type
+    }
+
+def save_file_mapping(url, bucket_name):
+    file_mapping = create_file_mapping(url, bucket_name)
+    db_file_result = file_db['fileMapping'].insert_one(file_mapping)
+    file_mapping['id'] = str(db_file_result.inserted_id)
+    print file_mapping.pop('_id')
+    return file_mapping
+
+
+@app.route("/file", methods=["POST"])
+def file_mapping():
+    form = request.form
+    if not form.has_key('url'):
+        return jsonify({'meta': {'msg': 'url is required key'}}), 400
+    bucket_name = "docs.turtlemint.com" if not form.has_key('bucketName') else form.get('bucketName')
+    return jsonify(save_file_mapping(form.get('url'), bucket_name))
 
 if __name__ == "__main__":
     app.run()
